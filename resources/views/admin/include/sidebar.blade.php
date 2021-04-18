82692abc2c968a22489aafa1fbd782695b0f36c4
Sangameshwar Melkunde <40599204+Sangameshwarmelkunde@users.noreply.github.com>
2021-04-18 14:27:50 +0530

    sidebar scroll

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -1,4 +1,4 @@
-   <div class="nk-sidebar">           
+    <div class="nk-sidebar">           
             <div class="nk-nav-scroll">
                 <ul class="metismenu" id="menu">
                     <li class="nav-label">Dashboard</li>
